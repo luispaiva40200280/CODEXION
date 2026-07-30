9f6af618922d0df39d302ccd9c75b5fbb15ffdb7
luispaiva40200280 <luispaiva8936@gmail.com>
2026-07-30 21:50:10 +0100

    start of the inicializaton of the coders and such

--- init.c
+++ init.c
@@ -9,5 +9,20 @@
 /*   Updated: 2026/07/30 03:51:48 by lpaiva           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "codexion.h"
 
+int	init_queue(t_data *data)
+{
+	data->queue = malloc(sizeof(t_heap));
+	if (!data->queue)
+		return (1);
+	data->queue->size = 0;
+	data->queue->coders = malloc(sizeof(t_coder *) * data->number_of_coders);
+	if (!data->queue->coders)
+		return (1);
+	data->queue->max_leng = data->number_of_coders;
+	data->queue->type = data->scheduler;
+	data->queue->data = data;
+	return (0);
+}
 
